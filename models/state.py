337e8ad542380b1af29bca9d40da041be46dce8f
bouhvli <bouhali.hamza.2@gmail.com>
2023-08-12 18:42:32 +0100

    remove the paychar

--- models/state.py
+++ models/state.py
@@ -4,5 +4,7 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """definition of class state has one attr is name"""
+    """definition of class state has one attr is name
+    Attr : 
+    name (str): state name"""
     name = ""
