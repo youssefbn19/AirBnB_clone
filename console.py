92069fc64d6bd866c5dad6522d65ce1b42f688e1
Youssef <ybenchiir@gmail.com>
2023-08-13 11:22:35 -0700

    Update and Add some methods

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ The module contains HBNBCommand class
 import cmd
 import models
 import ast
+import re
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -22,6 +23,25 @@ class HBNBCommand(cmd.Cmd):
     __classes = ["BaseModel", "Amenity", "City", "Place",
                  "Review", "State", "User"]
 
+    def default(self, line):
+        """
+        Checks special commands
+        """
+        reg = r'^(\w+)\.(\w+)\((.*)\)'
+        check_line = re.match(reg, line)
+        if check_line:
+            args = check_line.groups()
+            if args[1] == "all":
+                self.all(args[0])
+            elif args[1] == "count":
+                self.count(args[0])
+            elif args[1] == "show":
+                key = "{} {}".format(args[0], args[2].replace("\"", ""))
+                self.do_show(key)
+                # self.show(args[0], args[2])
+        else:
+            super().default(line)
+
     def command_args(self, line):
         """
         Check if the command arguments follow a certain instructions.
@@ -30,6 +50,7 @@ class HBNBCommand(cmd.Cmd):
         args = []
         if line:
             args = line.split()
+            print("coolab {}".format(args))
             if args[0] not in HBNBCommand.__classes:
                 print("** class doesn't exist **")
                 return []
@@ -44,11 +65,6 @@ class HBNBCommand(cmd.Cmd):
             clean_line = line.split()
             clean_line = ' '.join(clean_line)
         return clean_line
-
-    def emptyline(self):
-        """this function does nothing when pressing
-        empty line"""
-        pass
 
     def do_EOF(self, line):
         'Exit the program'
@@ -145,6 +161,61 @@ class HBNBCommand(cmd.Cmd):
                     instance = objs["{}.{}".format(args[0], args[1])]
                     instance.__dict__[args[2]] = value
                     instance.save()
+
+    def check_class(self, cls):
+        """
+        Checks if the given class is exist in the list of classes
+        """
+        if cls in HBNBCommand.__classes:
+            return True
+        else:
+            print("** class doesn't exist **")
+            return False
+
+    def list_instances(self, objs, cls):
+        """
+        Return a dictionary of instances of a specific class
+        """
+        instances = {key: value for key, value in objs.items()
+                     if key.startswith(cls)}
+        return instances
+
+    def all(self, cls_name):
+        """
+        Retrieve all instances of a given class
+        """
+        objects = models.storage.all()
+        if self.check_class(cls_name):
+            instances = self.list_instances(objects, cls_name)
+            result = [str(value) for value in instances.values()]
+            concat = ", ".join(result)
+            print("[{}]".format(concat))
+
+    def count(self, cls_name):
+        """
+        Retrieve the number of instances of a class
+        """
+        objects = models.storage.all()
+        if self.check_class(cls_name):
+            instances = self.list_instances(objects, cls_name)
+            print(len(instances))
+
+    # def show(self, cls_name, id=""):
+    #    """
+    #    Retrieve an instance based on its ID
+    #    """
+    #    objects = models.storage.all()
+    #    if self.check_class(cls_name):
+    #        if id:
+    #            instances = self.list_instances(objects, cls_name)
+    #            arg = "{}.{}".format(cls_name, id.replace("\"", ""))
+    #            print(arg)
+    #            if any(key == arg for key in instances):
+    #                print(instances[arg])
+    #            else:
+    #                print("** no instance found **")
+    #        else:
+    #            print("** instance id missing **")
 
 
 if __name__ == '__main__':
